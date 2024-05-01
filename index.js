91c582544367526096cf064e1f348fa958f0f568
Zachary Doherty <zacharyedoherty@gmail.com>
2024-05-01 13:43:03 -0400

    function notes

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Follow along with the examples here
+function doNothing() {}
+/*
+function sayHello() {
+        console.log("Hello!");
+      }
+sayHello();
+*/
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+function doSomething(thing) {
+    console.log(thing);
+  }
+doSomething('anything');
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane"); 
+sayHelloTo("R2-D2"); 
+sayHelloTo(1);
+function add(x, y) {
+    return x + y;
+  }
+console.log(add(1, 2));
